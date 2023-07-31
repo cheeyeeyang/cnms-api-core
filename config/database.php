643e5b10e4ccf965bc990138ee7d280964894ce2
Cheeyeeyang Mouasue <mouasuecheeyeeyang@gmail.com>
2023-07-31 07:39:29 +0700

    this is new update api for change order

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             // 'username' => env('DB_USERNAME', 'forge'),
             // 'password' => env('DB_PASSWORD', ''),
             // hosting
-            // 'url' => 'https://node49149-env-3419718.cslox-th.ruk-com.la/',
+            'url' => env('https://node49149-env-3419718.cslox-th.ruk-com.la/'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => 'cnms_26032023_db',
