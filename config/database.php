d134d9f60e325f135f153b4cc9dbb5a64d6c3851
Cheeyeeyang Mouasue <mouasuecheeyeeyang@gmail.com>
2023-07-31 14:17:48 +0700

    vdmfv

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             // 'password' => env('DB_PASSWORD', ''),
             // hosting
             'url' => env('https://node49149-env-3419718.cslox-th.ruk-com.la/'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => 'cnms_26032023_db',
             'username' => 'root',
